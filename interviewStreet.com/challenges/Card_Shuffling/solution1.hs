78432cae6e46370d5159e85cf2c00743c7732510
PushpRaj Agrawal <pushpraj.agrawal@gmail.com>
2012-08-03 18:25:57 +0530

    Card shuffle: Code previous solution again in haskell.

--- interviewStreet.com/challenges/Card_Shuffling/solution1.hs
+++ interviewStreet.com/challenges/Card_Shuffling/solution1.hs
@@ -1,31 +1,21 @@
 --- Read input from STDIN. Print output to STDOUT
+import Data.List (intersperse)
 
-import Data.Char
-import Data.List
-import Debug.Trace
+nextLoc k s i =  k - (quot i s) -1 + (rem i s) * k 
 
-breakIn _ [] = []
-breakIn size list =  (take size list) : (breakIn size (drop size list)) 
-breakInAct size list = reverse (breakIn size list)
+shuffle k s 0 count = count
+shuffle k s i count =  shuffle k s (nextLoc k s i) (count +1)
 
-
-shuffleOnce [] = []
-shuffleOnce (x:xs) = (head x) : (shuffleOnce ( xs ++ [tail x]))
-
-
---shuffle n k [1..n] = 0
---shuffle n n _ = 2
-shuffle n k list | list == [1..n] = 0
-		 | otherwise = 1 + (shuffle n k (shuffleOnce (breakInAct pileSize list)))
-				where pileSize = div n k 
+shuffleHelper n k = shuffle k s (nextLoc k s 0) 1
+			where s = div n k
 
 --testcase :: a -> a
 testcase [] = []
-testcase (cardSizeN : cardPileK : restList) = (shuffle cardSizeN cardPileK [1..cardSizeN]) : (testcase restList)
+testcase (cardSizeN : cardPileK : restList) = (shuffleHelper cardSizeN cardPileK) : (testcase restList)
 
 main = do
           input1 <- getContents
           let resultList = testcase (map read (tail (words input1)))
-          putStr (show resultList)
+          putStr . concat . intersperse "\n" . map show $ resultList
 
 
