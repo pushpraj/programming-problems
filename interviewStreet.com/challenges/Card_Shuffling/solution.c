ebf2bb5ca0064b22d49039baa7f54ff3d229205a
PushpRaj Agrawal <pushpraj.agrawal@gmail.com>
2012-08-03 17:57:47 +0530

    Card-Shuffle:Update solution.

--- interviewStreet.com/challenges/Card_Shuffling/solution.c
+++ interviewStreet.com/challenges/Card_Shuffling/solution.c
@@ -1,4 +1,5 @@
 
+#include <stdlib.h>
 
 int testcase() {
 	int N=0, K=0;
@@ -10,28 +11,27 @@ int testcase() {
 	int count = 0;
 	int index = 0;
 	do {
-	   int a = index / S;
-	   int b = index % S;
-
-	   index = (K-a) + b*K;
+//	  if( S>K ) {
+	   div_t val = div(index, S);
+	   index = (K-val.quot-1) + val.rem*K;
+//	  } else {
+//	   div_t val = div(index, K);
+//	   index = (-val.quot-1) + (val.rem+1)*S;
+//	  }
 	   count ++;
 	} while (index != 0);
 
-	return count;	
+	printf("%d\n", count);	
 }
 
 int main(void) {
 	int testcase_count = 0;
 	scanf("%d",&testcase_count);
-	int *ans_list = (int *) malloc( sizeof(int) * testcase_count);
 	
 	int i = 0, j =0;
 	for ( i=0; i< testcase_count; i++) {
-		ans_list[i] = testcase();
+		testcase();
 	}
 	
-	for ( j=0; j< testcase_count; j++) 
-		printf("%d\n", ans_list[j]);
-	free(ans_list);
 	return 0;
 }
