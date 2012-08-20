71084697fdc4a45ac152c68dcd249067cd83de14
PushpRaj Agrawal <pushpraj.agrawal@gmail.com>
2012-08-21 00:38:46 +0530

    Connected Set: a solution with equivalent sets. With bugs.

--- interviewStreet.com/amazon_challenge/Connected-Set/solution.c
+++ interviewStreet.com/amazon_challenge/Connected-Set/solution.c
@@ -8,12 +8,41 @@ enum loc {
     center
 };
 
-void markConnectedComponent ( int *array, int size, int index, int mark )
+#define setSize 100000
+int *directPtr, *actualSet, actualSetSize, directPtrSize;
+
+int connectComponents( int *array, int index, int indexKnown ) {
+  if (array[index] == 0 ) 
+    return 1;
+  
+  if ( array[indexKnown] == 1) {
+    array[ indexKnown] = array[index];
+    return 0;
+  }
+  
+  if ( array[index] == array[indexKnown]) {
+    return 0;
+  }
+  
+  actualSet[ directPtr[ array[index] ] ] = -1;
+  directPtr [ array[index] ] = directPtr[ array[indexKnown] ];
+  return 0;
+}
+
+void markIndex( int *array, int index ) {
+  actualSetSize++;
+  actualSet[actualSetSize] = actualSetSize;
+  directPtrSize++;
+  directPtr[directPtrSize] = actualSetSize;
+  array[ index ] = directPtrSize;
+}
+
+void markConnectedComponent ( int *array, int size, int index )
 {
     int locationX = center;
     int locationY = center; 
+  int isNew = 1;
     if (array[index] == 1) {
-        array[index] = mark;
         if ( index < size ) {
             locationY = top;
         } else if (index > size * (size-1)) {
@@ -28,76 +57,53 @@ void markConnectedComponent ( int *array, int size, int index, int mark )
         
         if (locationX==left)
             if (locationY == top) {
-                markConnectedComponent( array, size, index + 1, mark );
-                markConnectedComponent( array, size, index + size, mark );
-                markConnectedComponent( array, size, index + size + 1, mark );
-            } else if (locationY == bottom) {
-                markConnectedComponent( array, size, index + 1, mark );
-                markConnectedComponent( array, size, index - size, mark );
-                markConnectedComponent( array, size, index - size + 1, mark );
+              // empty
             } else {
-                markConnectedComponent( array, size, index + 1, mark );
-                markConnectedComponent( array, size, index + size, mark );
-                markConnectedComponent( array, size, index + size + 1, mark );
-                markConnectedComponent( array, size, index - size, mark );
-                markConnectedComponent( array, size, index - size + 1, mark );
+              isNew &= connectComponents( array, index - size + 1, index );
+              isNew &= connectComponents( array, index - size, index );
             }
         else if (locationX==right)
             if (locationY == top) {
-                markConnectedComponent( array, size, index - 1, mark );
-                markConnectedComponent( array, size, index + size, mark );
-                markConnectedComponent( array, size, index + size - 1, mark );
-            } else if (locationY == bottom) {
-                markConnectedComponent( array, size, index - 1, mark );
-                markConnectedComponent( array, size, index - size, mark );
-                markConnectedComponent( array, size, index - size - 1, mark );
+              isNew &= connectComponents( array, index - 1, index );
             } else {
-                markConnectedComponent( array, size, index - 1, mark );
-                markConnectedComponent( array, size, index + size, mark );
-                markConnectedComponent( array, size, index + size - 1, mark );
-                markConnectedComponent( array, size, index - size, mark );
-                markConnectedComponent( array, size, index - size - 1, mark );
+              isNew &= connectComponents( array, index - 1, index );
+              isNew &= connectComponents( array, index - size, index );
+              isNew &= connectComponents( array, index - size - 1, index );
             }
         else 
             if (locationY == top) {
-                markConnectedComponent( array, size, index - 1, mark );
-                markConnectedComponent( array, size, index + size, mark );
-                markConnectedComponent( array, size, index + 1, mark );
-                markConnectedComponent( array, size, index + size - 1, mark );
-                markConnectedComponent( array, size, index + size + 1, mark );
-            } else if (locationY == bottom) {
-                markConnectedComponent( array, size, index + 1, mark );
-                markConnectedComponent( array, size, index - 1, mark );
-                markConnectedComponent( array, size, index - size + 1, mark );
-                markConnectedComponent( array, size, index - size, mark );
-                markConnectedComponent( array, size, index - size - 1, mark );
+              isNew &= connectComponents( array, index - 1, index );
             } else {
-                markConnectedComponent( array, size, index - 1, mark );
-                markConnectedComponent( array, size, index + 1, mark );
-                markConnectedComponent( array, size, index + size, mark );
-                markConnectedComponent( array, size, index - size, mark );
-                markConnectedComponent( array, size, index + size - 1, mark );
-                markConnectedComponent( array, size, index + size + 1, mark );
-                markConnectedComponent( array, size, index - size + 1, mark );
-                markConnectedComponent( array, size, index - size - 1, mark );
+              isNew &= connectComponents( array, index - 1, index );
+              isNew &= connectComponents( array, index - size - 1, index );
+              isNew &= connectComponents( array, index - size, index );
+              isNew &= connectComponents( array, index - size + 1, index );
             }
-    } else if (array[index] != 0 && array[index] != mark)
-        // crash!
-        *((int *)0)=0;
+    }
+  
+    if (isNew)
+      markIndex( array, index);
 }
 
 int getComponent(int *array, int size) {
     int count = 0, i = 0;
+    actualSetSize = -1;
+    directPtrSize = 1;
     for (i=0; i<size*size; i++)
         if (array[i]==1){
-            markConnectedComponent( array, size, i, count+2 );
-            count ++;
-        }
+            markConnectedComponent( array, size, i);
+       }
+    for (i=0; i<actualSetSize+1; i++)
+      if (actualSet[i]!=-1)
+        count ++;
     return count;   
 }
 
 int main()
 {
+  directPtr = (int *)malloc(sizeof(int)*setSize);
+  actualSet = (int *)malloc(sizeof(int)*setSize);
+
     int size;
     int testcaseCount;
     int *array, i;
